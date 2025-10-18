b4581fc59802d545638d86a8508b0220547c3a09
Emily Johnson <38834202+kitcoun@users.noreply.github.com>
2025-10-19 01:02:03 +0800

    Update 00-secrets.sh

--- scripts/00-secrets.sh
+++ scripts/00-secrets.sh
@@ -4,7 +4,7 @@
 
 # 示例
 # 其他脚本使用 $LAN_IP 来引用
-# export LAN_IP="10.0.0.1"
-# export LAN_NETMASK="255.255.255.0"
+export LAN_IP="10.0.0.1"
+export LAN_NETMASK="255.255.255.0"
 # export PPPOE_USERNAME="your_pppoe_username"
 # export PPPOE_PASSWORD="your_pppoe_password"
